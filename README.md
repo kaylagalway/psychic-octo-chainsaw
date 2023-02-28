d6a7ef2d1447c5e1f21fec75a7e74422f633f60e
davidf <dfernandez06@gmail.com>
2023-02-27 19:30:56 -0500

    add rocket and some boilerplate

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ docker container exec -it <container name> /bin/bash
 
 STOP
 docker compose stop
+
+
+
+ROUTES
+GET index
+
+POST /login
+
+GET /user
+POST /user/abilities
+
+GET /feed
+GET /workout/:ID
+
+
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/kayla")]
+fn kdawg() -> &'static str {
+    "Hello Kayla"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let rock = rocket::build();
+    let r2 = rock.mount("/api", routes![index, kdawg]);
+    r2.mount("/", routes![index, kdawg])
 }
