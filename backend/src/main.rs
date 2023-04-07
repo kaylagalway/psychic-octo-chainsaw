41aceb5ecde162782e681bc22f6aa93e3b8655fd
davidf <dfernandez06@gmail.com>
2023-04-07 15:40:58 -0400

    this is a broken commit

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,10 @@
 #[macro_use] extern crate rocket;
+use rocket::Response;
+use rocket::http::{ContentType, Status};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::serde::json::Json;
+use rocket::Error;
+
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,6 +14,26 @@ fn index() -> &'static str {
 #[get("/kayla")]
 fn kdawg() -> &'static str {
     "Hello Kayla"
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct User {
+    message: String
+}
+
+#[post("/", format = "json", data = "<user_data>")]
+fn create_user(user_data: Result<Json<User>, Error>) -> Result<Response<'static>, Status> {
+    match user_data {
+        Ok(json) => {
+            let message = json.into_inner();
+            println!("received msg: {}", user_data.message);
+            Ok(Response::build())
+                .status(Status::Ok)
+                .header(ContentType::Plain)
+                .body("got it")
+        }   
+        Err(_) => Err(Status::BadRequest)
+    }
 }
 
 #[launch]
