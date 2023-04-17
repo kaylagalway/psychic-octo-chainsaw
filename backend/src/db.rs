6e569887525e695b8c1ba9f3895bcc061f88014a
davidf <dfernandez06@gmail.com>
2023-04-17 10:36:43 -0400

    connected to db

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,5 +1,14 @@
 use tokio_postgres::{NoTls, Error};
 
-pub fn main() {
-    println!("Davis rulz")
+#[tokio::main]
+pub async fn main() -> Result<(), Error> {
+    let (client, connection) = tokio_postgres::connect("host=localhost user=deff dbname=party", NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("aww shucks: {}", e);
+        }
+    });
+    
+    Ok(())
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,4 @@
 #[macro_use] extern crate rocket;
-
 mod db;
 mod api;
 
@@ -7,6 +6,10 @@ use api::{index, kdawg, create_user};
 
 #[launch]
 fn rocket() -> _ {
+    match db::main() {
+        Ok(v) => println!("db connected {v:?}"),
+        Err(e) => println!("fuck {e:?}")
+    }
     rocket::build()
         .mount("/", routes![index, kdawg, create_user])
 }
