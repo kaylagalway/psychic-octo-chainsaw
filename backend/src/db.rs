4ea2f3f79536dfcfba4d36e066b634f48d9cb74c
davidf <dfernandez06@gmail.com>
2023-04-17 10:43:59 -0400

    add dotenv

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,8 +1,13 @@
 use tokio_postgres::{NoTls, Error};
+use dotenv;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Error> {
-    let (client, connection) = tokio_postgres::connect("host=localhost user=deff dbname=party", NoTls).await?;
+    dotenv::dotenv().ok();
+    let db_host = dotenv::var("host").unwrap();
+    let db_user = dotenv::var("user").unwrap();
+    let db_name = dotenv::var("dbname").unwrap();
+    let (client, connection) = tokio_postgres::connect(&format!("host={} user={} dbname={}", db_host, db_user, db_name), NoTls).await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
